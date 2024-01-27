c48e7bc107be270d01862332a70014deb3362a53
Eric Donnelly <200433532@student.georgianc.on.ca>
2024-01-26 22:02:48 -0500

    finished part 2

--- part2/d.sql
+++ part2/d.sql
@@ -1,4 +1,8 @@
 -- Below is a dummy query, if we leave it empty then the SQLITE package will throw a non-readable error
 -- Change the query to pass the test :)
-select *
-from dogs;
+select first_name,
+    last_name
+from users
+    JOIN favorites ON users.id = favorites.user_id
+    JOIN dogs ON dogs.id = favorites.dog_id
+where dogs.name like "Zoey";
